51880e3277b425f5fecfce52ec08663cc2ca2c71
s-jqzhang <s-jqzhang@meizu.com>
2015-05-09 11:55:14 +0800

    0.2.2 loader

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='PyCodeigniter',
-    version='0.2.1',
-    packages=['', 'codeigniter', 'codeigniter.system', 'codeigniter.system.core', 'codeigniter.application',
+    version='0.2.2',
+    packages=[ 'codeigniter', 'codeigniter.system', 'codeigniter.system.core', 'codeigniter.application',
               'codeigniter.application.config', 'codeigniter.application.models',
               'codeigniter.application.controllers','codeigniter.application.helpers','codeigniter.application.library'],
     url='https://github.com/sjqzhang/PyCodeigniter',
     license='GPL',
+    install_requires = ['setuptools','pymysql','DBUtils'],
     requires=['pymysql','DBUtils','gevent'],
     author='s_jqzhang',
     author_email='s_jqzhang@163.com',
