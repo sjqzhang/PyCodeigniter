6dce4b878b6d2e47d6643f98db001f178db61df4
s-jqzhang <s-jqzhang@meizu.com>
2015-06-26 11:15:05 +0800

    mail attatch file

--- codeigniter/system/core/CI_Mail.py
+++ codeigniter/system/core/CI_Mail.py
@@ -4,7 +4,9 @@ __author__ = 'xiaozhang'
 
 
 import smtplib
+import os
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 
 
@@ -17,18 +19,27 @@ class CI_Mail(object):
 
 
 
-    def send(self,to,subject,content,is_html=True):
+    def send(self,to,subject,content,attachs=[]):
+        msg=MIMEMultipart('related')
         if isinstance(to,basestring):
             to=[to]
+        if isinstance(attachs,basestring):
+            attachs=[attachs]
         me=self.user+"<"+self.user+"@"+self.postfix+">"
-        if is_html:
-            msg = MIMEText(content,_subtype='html',_charset='utf-8')
-        else:
-            msg=content
+        tmsg = MIMEText(content,_subtype='html',_charset='utf-8')
         msg['Subject'] = subject
         msg['From'] = me
-
         msg['To'] = ";".join(to)
+        msg.attach(tmsg)
+        if len(attachs)>0:
+            for file_name in attachs:
+                if os.path.exists(file_name):
+                    att = MIMEText(open('%s'%file_name, 'rb').read(), 'base64', 'utf-8')#添加附件
+                    att["Content-Type"] = 'application/octet-stream'
+                    att["Content-Disposition"] = 'attachment; filename="%s"'% os.path.basename(file_name)
+                    msg.attach(att)
+                else:
+                    self.app.logger.warn('attach fail '+file_name+' not exist!')
         try:
             s = smtplib.SMTP()
             s.connect(self.host)
@@ -41,4 +52,14 @@ class CI_Mail(object):
             return False
 
 if __name__ == '__main__':
-    pass
+
+    conf={
+        'host':'smtp.163.com',
+        'user':'abc',
+        'password':'abc',
+        'postfix':'163.com'
+}
+    mail=CI_Mail(**conf)
+    mail.send('abc@163.com','你好','<h1>你为</h1>asdfasdfasf',[r'c:/test.txt'])
+
+
