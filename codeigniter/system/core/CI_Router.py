bcf1252f661240a7a5287e96813bc6cec5456ed6
s-jqzhang <s-jqzhang@meizu.com>
2015-10-15 10:52:50 +0800

    access log

--- codeigniter/system/core/CI_Router.py
+++ codeigniter/system/core/CI_Router.py
@@ -4,7 +4,10 @@ __author__ = 'xiaozhang'
 
 import json
 import re
-
+import time
+import logging
+from logging.handlers import RotatingFileHandler
+import datetime
 
 try:
     import web,cgi,os
@@ -13,10 +16,34 @@ except Exception as e:
 
 
 
+
+
+
+
 class CI_Router(object):
     def __init__(self,**kwargs):
         self.app=kwargs['app']
+        self.config=self.app.config
+        self.access_log=logging.getLogger('access_log')
+        self.error_log=logging.getLogger('error_log')
+        self.access_log.setLevel(logging.DEBUG)
+        self.error_log.setLevel(logging.DEBUG)
+        if 'access_log' in self.config['server']:
+            self.access_log_file= self.config['server']['access_log']
+        else:
+            self.access_log_file='./access.log'
+        if 'error_log' in self.config['server']:
+            self.error_log_file= self.config['server']['error_log']
+        else:
+            self.error_log_file='./error.log'
         self._methods={}
+        acchandler = RotatingFileHandler(filename=self.access_log_file, maxBytes=1024*1024*1024, backupCount=10)
+        errhandler = RotatingFileHandler(filename=self.access_log_file, maxBytes=1024*1024*1024, backupCount=10)
+        format='%(asctime)s %(message)s'
+        acchandler.setFormatter(format)
+        errhandler.setFormatter(format)
+        self.access_log.addHandler(acchandler)
+        self.error_log.addHandler(errhandler)
 
 
     def get_func(self,ctrl,func):
@@ -55,10 +82,20 @@ class CI_Router(object):
 
 
 
+    def _log(self,env,code,etime):
+        now = datetime.datetime.now()
+        dt=now.strftime('%Y-%m-%d %H:%M:%S')
+        method=env['REQUEST_METHOD']
+        addr=env['REMOTE_ADDR']
+        path=env['PATH_INFO']
+        protol=env['SERVER_PROTOCOL']
+        message="'%s - - [%s] %s %s %s %s %s'" % (addr,dt,method,path,protol,str(code),etime)
+        self.access_log.info(message)
 
 
 
     def wsgi_route(self,env):
+        stime=time.time()
         data=self.app.input.parse(env)
         try:
             if '__ctrl_name__' in data.keys():
@@ -78,7 +115,10 @@ class CI_Router(object):
         except Exception as err:
             return "404 Not Found", "Not Found"
         try:
-            return '200 OK',eval('self.app.loader.ctrl(ctrl).'+func+'(**data)')
+            ret=eval('self.app.loader.ctrl(ctrl).'+func+'(**data)')
+            etime=time.time()
+            self._log(env,etime-stime)
+            return '200 OK',ret
         except TypeError as e:
             self.app.logger.error('when call controller %s function %s error,%s'%(ctrl,func,str(e)))
             return '200 OK',{'message':str(e),'code':500}
