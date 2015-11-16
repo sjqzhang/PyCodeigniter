2c12ffd1198000dcb96040064aeb4fda66f8968e
s-jqzhang <s-jqzhang@meizu.com>
2015-11-16 18:10:32 +0800

    zookeeper

--- codeigniter/system/core/CI_Zookeeper.py
+++ codeigniter/system/core/CI_Zookeeper.py
@@ -4,12 +4,20 @@ __author__ = 'xiaozhang'
 
 from kazoo.client import KazooClient
 from kazoo.client import KazooState
+import threading
+import time
+
 
 class CI_Zookeeper(object):
     def __init__(self,**kwargs):
         self.zk_conf=kwargs['zookeeper']
         self.app=kwargs['app']
+        self.election=None
+        self._is_leader=False
         self.init()
+
+
+
 
     def init(self):
         self.url=self.zk_conf['url']
@@ -18,7 +26,10 @@ class CI_Zookeeper(object):
         self.zk=zk = KazooClient(hosts=self.url,timeout=self.zk_conf['timeout'])
         self.zk.start()
         self.zk.ensure_path(self.zk_conf['path'])
+        self.election = zk.Election(self.zk_conf['path'])
         self.zk.add_listener(self.zk_listener)
+        threading.Thread(target=self._noblock).start()
+        time.sleep(0.2)
 
 
     def zk_listener(self,state):
@@ -30,6 +41,32 @@ class CI_Zookeeper(object):
         else:
             pass
             # Handle being connected/reconnected to Zookeeper
+
+
+    def _jump(self):
+        while True:
+            self._is_leader=True
+            time.sleep(0.5)
+
+    def _noblock(self):
+        while True:
+            self.election.run(self._jump)
+            self._is_leader=False
+
+
+    def is_leader(self):
+        return self._is_leader
+
+    def is_leader2(self,callback,*args,**kwargs):
+        self.election.run(callback,*args,**kwargs)
+
+
+
+
+
+
+
+
 
 
 
@@ -53,4 +90,42 @@ if __name__=='__main__':
 }
     zkconf={'zookeeper':zookeeper,'app':'app'}
     zk=CI_Zookeeper(**zkconf)
+    import time
+    def  test():
+        time.sleep(5)
+        print('hello wlorl')
+        time.sleep(50)
+    # time.sleep(2)
+    while True:
+        print zk.is_leader()
+        time.sleep(1)
+    # time.sleep(50)
+    print 'sdfasdf'
 
+
+
+from kazoo.client import KazooClient
+import time
+import uuid
+
+import logging
+logging.basicConfig()
+
+my_id = uuid.uuid4()
+
+def leader_func():
+    print "I am the leader {}".format(str(my_id))
+    while True:
+        print "{} is working! ".format(str(my_id))
+        time.sleep(3)
+
+zk = KazooClient(hosts='127.0.0.1:2181')
+zk.start()
+
+election = zk.Election("/electionpath")
+
+# blocks until the election is won, then calls
+# leader_func()
+election.run(leader_func)
+
+zk.stop()
