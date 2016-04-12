2d2afccf746e8dab66d182a4570637f4e86fa720
s-jqzhang <s-jqzhang@meizu.com>
2016-04-12 14:35:11 +0800

    cli

--- Cli.py
+++ Cli.py
@@ -16,7 +16,14 @@ class Cli:
 
 
     def help(self,param=''):
-        return "hello world"
+	h='''
+        cli upgrade   更新 cli 程序
+        cli shell   下载并接行shell指令
+        cli listfile   查看文件列表
+        cli download   下载文件
+        cli delfile -f filename -k key  删除文件
+         '''
+        return h
 
     def download(self,file=''):
         return open('files'+os.path.sep+ file,'rb').read()
@@ -24,6 +31,8 @@ class Cli:
     def shell(self,file='',param=''):
         return open('files'+os.path.sep+ file,'rb').read()
 
+    def upgrade(self,param=''):
+        return open('cli').read()
 
     def _params(self,param='{}',opts=''):
         params= json.loads(param)
@@ -34,7 +43,6 @@ class Cli:
 
 
     def upload(self,**kwargs):
-        print kwargs
         file=kwargs['file']
         filename='files/'+file.filename
         if not os.path.exists(filename):
@@ -49,7 +57,7 @@ class Cli:
     def delfile(self,param=''):
         params=self._params(param)
         filename=''
-        key='Mz'
+        key='xxx'
         k=''
         if  'f' in params:
             filename=params['f']
@@ -67,3 +75,4 @@ class Cli:
             return "sucess"
         else:
             return "Not Found"
+
