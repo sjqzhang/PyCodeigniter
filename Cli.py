15aa9a1c2d0a87d6c99b8330bd22f0330d1e8e59
s-jqzhang <s-jqzhang@meizu.com>
2016-04-15 11:52:25 +0800

    clid

--- Cli.py
+++ Cli.py
@@ -22,6 +22,13 @@ class Cli:
         cli listfile   查看文件列表
         cli download   下载文件
         cli delfile -f filename -k key  删除文件
+
+        cli addenv -k key -v value  -g group (default)  增加环境变量
+        cli getevn  -k key -g grup (default) 获取环境变量
+        cli delenv   -k key -g grup 删除环境变量 
+        cli listenv   -g grup 查看某个组的环境变量 默认 default
+        cli updateenv   -k key -v value -g group (default)更新环境变量 
+
          '''
         return h
 
@@ -42,37 +49,134 @@ class Cli:
         return "\n".join(os.listdir('files'))
 
 
-    def upload(self,**kwargs):
-        file=kwargs['file']
-        filename='files/'+file.filename
-        if not os.path.exists(filename):
-            if isinstance(file,str):
-                open(filename,'wb').write(file)
-            else:
-                open('files/'+file.filename,'wb').write(file.file.read())
-            return 'success'
-        else:
-            return 'file exists'
+    def upload(self,**kwargs): 
+        file=kwargs['file'] 
+        filename='files/'+file.filename 
+        if not os.path.exists(filename): 
+            if isinstance(file,str): 
+                open(filename,'wb').write(file) 
+            else: 
+                open('files/'+file.filename,'wb').write(file.file.read()) 
+            return 'success' 
+        else: 
+            return 'file exists' 
+ 
+    def delfile(self,param=''): 
+        params=self._params(param) 
+        filename='' 
+        key='meizu.com' 
+        k='' 
+        if  'f' in params: 
+            filename=params['f'] 
+        else: 
+            return '-f(filename) require' 
+        if  'k' in params: 
+            k=params['k'] 
+        else: 
+            return '-k(key) require' 
+        if not key==k: 
+            return 'key error' 
+        path='files/'+filename 
+        if os.path.exists(path): 
+            os.remove(path) 
+            return "sucess" 
+        else: 
+            return "Not Found" 
 
-    def delfile(self,param=''):
+################################################env###############################################
+    def _checkenv(self,param=''):
         params=self._params(param)
-        filename=''
-        key='xxx'
-        k=''
-        if  'f' in params:
-            filename=params['f']
-        else:
-            return '-f(filename) require'
-        if  'k' in params:
-            k=params['k']
+        key=''
+        group='default'
+        if 'g' in params:
+            group=params['g']
+        if 'k' in params:
+            key=params['k']
         else:
             return '-k(key) require'
-        if not key==k:
-            return 'key error'
-        path='files/'+filename
-        if os.path.exists(path):
-            os.remove(path)
-            return "sucess"
+        rows=ci.db.query("select * from env where `group_`='%s' and `key_`='%s'"% (group,key))
+        if len(rows)>0:
+            return True
         else:
-            return "Not Found"
-
+            return False
+    def listenv(self,param=''):
+        params=self._params(param)
+        group='default'
+        if 'g' in params:
+            group=params['g']
+        sql="select key_,value_ from env where `group_`='%s'" % (group)
+        rows=ci.db.query(sql)
+        if len(rows)==0:
+            return '(error) not found'
+        ret=''
+        for row in rows:
+            ret+=row['key_']+'='+row['value_']+"\n"
+        return ret
+    def updateenv(self,param=''):
+        params=self._params(param)
+        key=''
+        value=''
+        group='default'
+        if 'k' in params:
+            key=params['k']
+        else:
+            return '-k(key) require'
+        if 'g' in params:
+            group=params['g']
+        if 'v' in params:
+            value=params['v']
+        else:
+            return '-v(value) require'
+        if not self._checkenv(param):
+            return '(error)key not is exsit'  
+        ci.db.update('env',{'value_':value},{'key_':key,'group_':group})
+        return 'ok'
+            
+    def addenv(self,param=''):
+        params=self._params(param)
+        key=''
+        value=''
+        
+        group='default'
+        if 'k' in params:
+            key=params['k']
+        else:
+            return '-k(key) require'
+        if 'g' in params:
+            group=params['g']
+        if 'v' in params:
+            value=params['v']
+        else:
+            return '-v(value) require'
+        if self._checkenv(param):
+            return '(error)key is exsit'  
+        ci.db.insert('env',{'key_':key,'value_':value,'group_':group})
+        return 'ok'
+    def delenv(self,param=''):
+        params=self._params(param)
+        key=''
+        group='default'
+        if 'g' in params:
+            group=params['g']
+        if 'k' in params:
+            key=params['k']
+        else:
+            return '-k(key) require'
+        if self._checkenv(param):
+            ci.db.delete('env',{'key_':key,'group_':group})
+            return 'ok'
+        else:
+            return '(error)key no found'
+    def getenv(self,param=''):
+        params=self._params(param)
+        key=''
+        group='default'
+        if 'g' in params:
+            group=params['g']
+        if 'k' in params:
+            key=params['k']
+        else:
+            return '-k(key) require'
+        if not self._checkenv(param):
+            return '(error)key no found'
+        return ci.db.scalar("select value_ from env where `group_`='%s' and `key_`='%s'"% (group,key))['value_']
