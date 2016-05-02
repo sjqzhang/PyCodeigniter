225e24e58b9ba18d13c61a4cfadbef01153beba4
s-jqzhang <s-jqzhang@meizu.com>
2016-05-02 13:55:39 +0800

    cli

--- Cli.py
+++ Cli.py
@@ -9,14 +9,37 @@ import os
 import json
 import re
 import sys
+import time
+import base64
+
+
+
+def auth(func):
+    def decorated(*arg,**kwargs):
+        if not 'HTTP_AUTH_UUID' in ci.local.env:
+            return "(error)unauthorize1"
+        print ci.local.env['HTTP_AUTH_UUID']
+        print ci.cache.get(ci.local.env['HTTP_AUTH_UUID'])
+        if ci.cache.get(ci.local.env['HTTP_AUTH_UUID'])==None:
+            return "(error)unauthorize"
+        return func(*arg,**kwargs)
+    return decorated
+
+
+
+
 
 
 
 class Cli:
 
-
-    def index(self):
-        return "hello world"
+    @auth
+    def index(self,param=''):
+        print ci.local.env
+        #ci.set_header('WWW-Authenticate','Basic realm="Authentication System"')
+        #ci.set_header('HTTP/1.0 401 Unauthorized')
+        # sys.exit(0)
+        return "hello world".strip()
 
 
     def help(self,param=''):
@@ -29,24 +52,83 @@ class Cli:
         cli upload -f filename  上传文件
         cli download -f filename  下载文件
         cli delfile -f filename -k key  删除文件
-        
+
         ########## 环境变量 ##############
 
         cli addenv -k key -v value  -g group (default)  增加环境变量
         cli getevn  -k key -g group (default) 获取环境变量
-        cli delenv   -k key -g group 删除环境变量 
+        cli delenv   -k key -g group 删除环境变量
         cli listenv   -g group -e 1 查看某个组的环境变量 默认 default -e 1 导出
-        cli updateenv   -k key -v value -g group (default)更新环境变量 
+        cli updateenv   -k key -v value -g group (default)更新环境变量
 
          '''
         return h
 
-    def download(self,file=''):
-        if  'f' in params: 
-            filename=params['f'] 
-        else: 
-            return '-f(filename) require' 
-        return open('files'+os.path.sep+ file,'rb').read()
+    def disableuser(self,param=''):
+        return self._userstatus(param,0)
+    def enableuser(self,param=''):
+        return self._userstatus(param,1)
+    def _userstatus(self,param='',status=0):
+        params=self._params(param)
+        user=''
+        uuid='(error) not login'
+        if  'u' in params:
+            user=params['u']
+        else:
+            return '-u(user) require'
+        data={'user':user,'status':status}
+        ci.db.query("update user set status='{status}' where user='{user}'",data)
+        return 'success'
+
+    def register(self,param=''):
+            params=self._params(param)
+            user=''
+            pwd=''
+            uuid='(error) not login'
+            if  'u' in params:
+                user=params['u']
+            else:
+                return '-u(user) require'
+            if  'p' in params:
+                pwd=params['p']
+            else:
+                return '-p(password) require'
+            data={'user':user}
+
+            if ci.db.scalar("select count(1) as cnt from user where user='{user}'",data)['cnt']>0:
+                return "(error)user exist"
+            data={'user':user,'pwd':ci.md5(pwd) }
+            ci.db.query("insert into user(user,pwd) values('{user}','{pwd}')",data)
+            return 'success'
+
+    def login(self,param=''):
+        params=self._params(param)
+        user=''
+        pwd=''
+        ip=''
+        uuid='(error) user or password error '
+        if  'u' in params:
+            user=params['u']
+        else:
+            return '-u(user) require'
+        if  'p' in params:
+            pwd=params['p']
+        else:
+            return '-p(password) require'
+        if  'i' in params:
+            ip=params['i']
+        data={'user':user,'pwd':ci.md5(pwd),'status':1}
+        is_exist=ci.db.scalar("select count(1) as cnt from user where user='{user}' and pwd='{pwd}' and status='{status}' limit 1 offset 0",data)['cnt']
+        udata={'user':user,'lasttime':time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(time.time())),'ip':ip}
+        if is_exist>0:
+            ci.db.query("update user set logincount=logincount+1,lasttime='{lasttime}',ip='{ip}' where user='{user}'",udata)
+            uuid=str(ci.uuid())
+            ci.cache.set(uuid,uuid)
+        else:
+            ci.db.query("update user set logincount=logincount+1,failcount=failcount+1,lasttime='{lasttime}',ip='{ip}' where user='{user}'",udata)
+
+        return str(uuid)
+
 
     def shell(self,file='',param=''):
         return open('files'+os.path.sep+ file,'rb').read()
@@ -59,7 +141,7 @@ class Cli:
         return params
 
     def listfile(self,param=''):
-        params=self._params(param) 
+        params=self._params(param)
         if 'd' in params:
             directory=params['d']
         else:
@@ -68,48 +150,48 @@ class Cli:
         return "\n".join(os.listdir('files/'+directory))
 
 
-    def upload(self,**kwargs): 
-        file=kwargs['file'] 
-        directory=kwargs['dir'] 
+    def upload(self,**kwargs):
+        file=kwargs['file']
+        directory=kwargs['dir']
         directory=directory.replace('.','')
         path='files/'+directory
-        filename=path+'/'+file.filename 
+        filename=path+'/'+file.filename
         if not os.path.isdir(path):
             os.mkdir(path)
-        if not os.path.exists(filename): 
-            if isinstance(file,str): 
-                open(filename,'wb').write(file) 
-            else: 
-                open(filename,'wb').write(file.file.read()) 
-            return 'success' 
-        else: 
-            return 'file exists' 
- 
-    def delfile(self,param=''): 
-        params=self._params(param) 
-        filename='' 
-        key='meizu.com' 
+        if not os.path.exists(filename):
+            if isinstance(file,str):
+                open(filename,'wb').write(file)
+            else:
+                open(filename,'wb').write(file.file.read())
+            return 'success'
+        else:
+            return 'file exists'
+
+    def delfile(self,param=''):
+        params=self._params(param)
+        filename=''
+        key='meizu.com'
         directory='/'
-        k='' 
-        if  'f' in params: 
-            filename=params['f'] 
-        else: 
-            return '-f(filename) require' 
-        if  'k' in params: 
-            k=params['k'] 
-        else: 
-            return '-k(key) require' 
-        if  'd' in params: 
-            directory=params['d'] 
-        if not key==k: 
-            return 'key error' 
+        k=''
+        if  'f' in params:
+            filename=params['f']
+        else:
+            return '-f(filename) require'
+        if  'k' in params:
+            k=params['k']
+        else:
+            return '-k(key) require'
+        if  'd' in params:
+            directory=params['d']
+        if not key==k:
+            return 'key error'
         directory=directory.replace('.','')
-        path='files/'+directory + '/' +filename 
-        if os.path.exists(path): 
-            os.remove(path) 
-            return "sucess" 
-        else: 
-            return "Not Found" 
+        path='files/'+directory + '/' +filename
+        if os.path.exists(path):
+            os.remove(path)
+            return "sucess"
+        else:
+            return "Not Found"
     def rexec(self,param=''):
         params=self._params(param)
         ip=''
@@ -198,7 +280,7 @@ class Cli:
             if row['group_']!=None:
                 ret+=str(row['group_'])+"\n"
         return ret
-                
+
         return ret
     def listenv(self,param=''):
         params=self._params(param)
@@ -214,11 +296,11 @@ class Cli:
             return '(error) not found'
         ret=''
         for row in rows:
-            if export=='1': 
+            if export=='1':
                 ret+='export '+row['key_']+'='+row['value_']+"\n"
             else:
                 ret+=row['key_']+'='+row['value_']+"\n"
-                
+
         return ret
     def updateenv(self,param=''):
         params=self._params(param)
@@ -236,15 +318,15 @@ class Cli:
         else:
             return '-v(value) require'
         if not self._checkenv(param):
-            return '(error)key not is exsit'  
+            return '(error)key not is exsit'
         ci.db.update('env',{'value_':value},{'key_':key,'group_':group})
         return 'ok'
-            
+
     def addenv(self,param=''):
         params=self._params(param)
         key=''
         value=''
-        
+
         group='default'
         if 'k' in params:
             key=params['k']
@@ -257,7 +339,7 @@ class Cli:
         else:
             return '-v(value) require'
         if self._checkenv(param):
-            return '(error)key is exsit'  
+            return '(error)key is exsit'
         ci.db.insert('env',{'key_':key,'value_':value,'group_':group})
         return 'ok'
     def delenv(self,param=''):
@@ -288,7 +370,7 @@ class Cli:
         if not self._checkenv(param):
             return '(error)key no found'
         return ci.db.scalar("select value_ from env where `group_`='%s' and `key_`='%s'"% (group,key))['value_']
-        
+
 ################################################ doc ###############################################
     def _checkdoc(self,param=''):
         params=self._params(param)
@@ -313,9 +395,9 @@ class Cli:
             if row['cmd']!=None:
                 ret+=unicode(row['cmd'])+"\n"
         return ret
-                
+
         return ret
-            
+
     def adddoc(self,param=''):
         params=self._params(param)
         cmd=''
@@ -331,16 +413,16 @@ class Cli:
             return '-d(document) require'
         if 'r' in params:
             remark=params['r']
-        sql='''INSERT INTO doc 
+        sql='''INSERT INTO doc
 	(
-	cmd, 
-	doc, 
+	cmd,
+	doc,
 	remark
 	)
 	VALUES
 	(
-	'{cmd}', 
-	'{doc}', 
+	'{cmd}',
+	'{doc}',
 	'{remark}'
 	)'''
         ci.db.query(sql,{'cmd':cmd,'doc':doc,'remark':remark})
@@ -379,4 +461,4 @@ class Cli:
                     ret+=str(row['doc'])+"\n"*3
                 #ret+="#"*50+"\n"
         return ret
-        
+
